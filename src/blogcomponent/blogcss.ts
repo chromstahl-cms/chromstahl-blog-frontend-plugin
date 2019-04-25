ab148e13f46436ce5b4314564a6b276f13d58707
fred <fredlahde@gmail.com>
2019-04-25 13:13:31 +0200

    Add css for blog posts

--- src/blogcomponent/blogcss.ts
+++ src/blogcomponent/blogcss.ts
@@ -43,4 +43,39 @@ export const css = `
     margin-left: auto;
     color: #d1d1d1;
 }
+
+.blogTextContainer ul, .blogTextContainer ol {
+  padding-left: 30px;
+}
+
+.blogTextContainer blockquote {
+  padding-left: 1em;
+  border-left: 3px solid #eee;
+  margin-left: 0; margin-right: 0;
+}
+
+.blogTextContainer img {
+  cursor: default;
+}
+
+.blogTextContainer h5 {
+  margin: 0;
+  font-weight: normal;
+  font-size: 100%;
+  color: #444;
+}
+
+.blogTextContainer hr {
+  padding: 2px 10px;
+  border: none;
+  margin: 1em 0;
+}
+
+.blogTextContainer hr:after {
+  content: "";
+  display: block;
+  height: 1px;
+  background-color: silver;
+  line-height: 2px;
+}
 `
